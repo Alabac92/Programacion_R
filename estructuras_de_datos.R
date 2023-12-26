829e69fe1b4b62846ad2086465ea3e98e5abe6d1
alfredo_aburto <95927418+Alabac92@users.noreply.github.com>
2023-12-25 18:44:43 -0600

    Estructuras de datos - Listas
    Agregamos descripcion de definicion y uso de las listas en R
    usando ejemplos sencillos.

--- estructuras_de_datos.R
+++ estructuras_de_datos.R
@@ -50,3 +50,34 @@ x
 x["c"]
 # Con lo anterior enviamos por pantalla al elemento c del
 # vector x
+
+# LISTAS
+# Las listas difieren de los vectores atómicos porque sus
+# elementos pueden ser de cualquier tipo. Además las listas
+# pueden contener a otras listas lo que se conoce como matrices
+
+# Definición de una lista utilizando la funcion list()
+lista_01 <- list("alfredo", 17L, 8.3, FALSE)
+
+# Definición de listas anidadas
+lista_02 <- list(2L, 3.9273, "a", TRUE, list("b", FALSE))
+
+# Determinar la estructura de una lista
+# Si se desea saber qué tipos de elementos contiene una lista,
+# puedes utilizar la función str().
+str(lista_02)
+
+# Al ejecutar la función str(), R mostrará la estructura de
+# datos de la lista mediante la descripción de sus elementos
+# y tipos
+
+# Nombrando los elementos de una lista
+# Al igual que con los vectores, podemos nombrar los elementos
+# de una lista, asignandole a cada elemento un nombre cuando
+# definimos la lista
+
+lista_03 = list('Temperatura (K)' = 20.5, "Presion (atm)" = 1L)
+
+# Enviamos por pantalla lista_03
+lista_03
+
