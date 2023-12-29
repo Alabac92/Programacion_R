d4c4f6836d9aea31aecaecf9c62cef522abe3cab
alfredo_aburto <95927418+Alabac92@users.noreply.github.com>
2023-12-28 19:16:29 -0600

    visualizacion_de_datos
    Agregamos el fichero visualizacion_de_datos con una introduccion a lo
    que se refiere el tema. Mencionando algunos paquetes utiles para la
    visualizacion de datos en R

---
+++ visualizacion_de_datos.R
@@ -0,0 +1,11 @@
+# Elementos basicos de visualizacion 
+
+# En R existen multiples paquetes que permiten crear visualizacion de datos.
+# Plotly te permite realizar una gran variedad de funciones de visualización,
+# por otro lado, RGL se enfoca en soluciones específicas como visualizaciones 3D.
+
+# Algunos de los paquetes más populares incluyen:
+# ggplot2, Plotly, Lattice, RGL, Dygraphs, Leaflet, Highcharter, Patchwork, 
+# gganimate y ggridges. 
+
+# ggplot2
