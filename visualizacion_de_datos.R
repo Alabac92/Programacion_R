9a2c0de534adc9b92b11ed0eaf297e8a14de9b89
alfredo_aburto <95927418+Alabac92@users.noreply.github.com>
2023-12-28 19:40:11 -0600

    Proceso de creacion de un diagrama
    Incluimos la descripcion de la creacion de un diagrama usando
    ggplot2, explicando las partes fundamentales de un bloque de
    codigo que ejemplifica la creacion de un diagrama de dispersion
    de puntos usando el data set de pinguinos.

--- visualizacion_de_datos.R
+++ visualizacion_de_datos.R
@@ -57,3 +57,51 @@
 # Por último, las funciones label y annotate te dejan personalizar tu diagrama. 
 # Puedes agregar texto como títulos, subtítulos y leyendas para comunicar el
 # propósito de tu diagrama o destacar datos importantes.
+
+# Proceso de creación de un diagrama
+
+# 1. Preparar datps
+# El paquete ggplot2 te permite usar código R para especificar el conjunto de 
+# datos, la figura geométrica y la estética de tu diagrama.
+# Para hacerlo, primero elige un conjunto de datos con el que trabajar. 
+library(ggplot2)
+library(palmerpenguins)
+data("penguins")
+View(penguins)
+
+# 2. Creación del diagrama
+
+# Supongamos que quieres modelar la relación entre masa corporal y longitud de
+# aletas en las tres especies de pingüino. Puedes elegir una figura geométrica
+# específica que se adapte al tipo de datos que tienes. Los puntos muestran la
+# relación entre dos variables cuantitativas. Un diagrama de dispersión de puntos
+# sería una manera eficaz de mostrar la relación entre las dos variables. 
+# Puedes colocar longitud de aleta en el eje X y masa corporal en el eje Y.
+
+ggplot(data = penguins) +geom_point(mapping = aes(x = flipper_length_mm, 
+                                                  y = body_mass_g))
+
+# ggplot(data = penguins):En ggplot 2, comienzas un diagrama con la función
+# ggplot(). La función ggplot() crea un sistema de coordenadas al que puedes 
+# agregar capas. El primer argumento de la función ggplot() es el conjunto de 
+# datos a usar en el diagrama. En este caso, es “penguins”.
+
+# +: Luego agregas un símbolo “+” para agregar una nueva capa a tu diagrama. 
+# Completas el diagrama agregando una o más capas a ggplot().
+
+# geom_point(): Luego eliges una figura geométrica agregando una función 
+# geométrica. La función geom_point() usa puntos para crear diagramas de 
+# dispersión, la función geom_bar usa barras para crear gráficos de barras, etc.
+# En este caso, elige la función geom_point para crear un diagrama de dispersión 
+# de puntos. El paquete ggplot2 viene con muchas funciones geométricas diferentes. 
+
+# (mapping = aes(x = flipper_length_mm, y = body_mass_g)): Cada función geométrica
+# en ggplot2 toma un argumento de mapeo. Esto define cómo se aplican variables 
+# de tu conjunto de datos a propiedades visuales. El argumento de mapeo siempre
+# se utiliza en conjunto con la función aes(). Los argumentos X e Y de la 
+# función aes() especifican qué variables aplicar al eje X y al eje Y del 
+# sistema de coordenadas. En este caso, quieres aplicar la variable 
+# “flipper_length_mm” al eje X y la variable “body_mass_g” al eje Y.
+
+# El diagrama muestra una relación positiva entre las dos variables.
+# Es decir, cuanto más grande es el pingüino, más larga es la aleta.
